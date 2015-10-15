575399b90c726d839852c7909b22186c759a7885
hjeannin <hjeannin@student.42.fr>
2015-10-15 16:24:53 +0200

    retour a la ligne en trop

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,10 @@ int				main()
 
 	std::cout << my_grid;
 	my_grid.print();
-	std::cout << std::endl << "IN (2, 2) : " << my_grid.access(2, 2) << std::endl;
-	std::cout << std::endl << "IN (0, 2) : " << my_grid.access(0, 2) << std::endl;
-	std::cout << std::endl << "IN (1, 2) : " << my_grid.access(1, 2) << std::endl;
-	std::cout << std::endl << "IN (0, 0) : " << my_grid.access(0, 0) << std::endl;
+	std::cout << std::endl << "IN (2, 2) : " << my_grid.access(2, 2);
+	std::cout << std::endl << "IN (0, 2) : " << my_grid.access(0, 2);
+	std::cout << std::endl << "IN (1, 2) : " << my_grid.access(1, 2);
+	std::cout << std::endl << "IN (0, 0) : " << my_grid.access(0, 0);
 	std::cout << std::endl << "IN (2, -1) : " << my_grid.access(2, -1) << std::endl;
 	return (0);
 }
